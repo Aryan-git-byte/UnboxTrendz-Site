1a1c6d4ffe84f2b542ee1f34cc67f7cb721d18e3
Aryan Kumar <aryan17550@gmail.com>
2025-09-12 20:46:29 +0530

    Updated AdminDashboard.tsx

--- src/pages/admin/AdminDashboard.tsx
+++ src/pages/admin/AdminDashboard.tsx
@@ -695,12 +695,13 @@ export default function AdminDashboard() {
                           >
                             <Eye className="h-4 w-4" />
                           </button>
-                         <button
+<button
   onClick={() => handleDeleteOrder(order.id)}
   className="text-red-600 hover:text-red-900 p-1 rounded hover:bg-red-50"
   title="Delete order"
 >
   <Trash2 className="h-4 w-4" />
 </button>
+
 
                         
